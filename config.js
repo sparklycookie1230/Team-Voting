45ece6b5f4a7c28108ce7cd74999360b7c51d787
sparklycookie1230 <seemasaraf23@gmail.com>
2022-01-08 18:26:53 +0000

    added the config key

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+    const firebaseConfig = {
+        apiKey: "AIzaSyB_TMJf1KLQzFDg1ZDT1bYQ5JfZKgCeEPs",
+        authDomain: "class67-4dea3.firebaseapp.com",
+        projectId: "class67-4dea3",
+        storageBucket: "class67-4dea3.appspot.com",
+        messagingSenderId: "175475288773",
+        appId: "1:175475288773:web:81229bfafa866a64ed1eac"
+      };
+      
+      // Initialize Firebase 
 
-};
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
